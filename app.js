b293769c168dd4e4273360d9c234917a7b07c667
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-03-14 10:42:14 +0530

    added new files

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var employee = {
+    name: "Rajesha Maravanthe",
+    age: "28",
+    salary: "1500000 per annum",
+    address: {
+        city: "Kundapura",
+        state: "Karnataka",
+        pin: "576224"
+    }
+}
+
+console.log("Name: "+ employee.name);
+console.log("Age: "+ employee.age);
+console.log("Salary: "+ employee.salary);
+console.log("City: "+ employee.address.city);
+console.log("State: "+ employee.address.state);
+console.log("Pin: "+ employee.address.pin);
